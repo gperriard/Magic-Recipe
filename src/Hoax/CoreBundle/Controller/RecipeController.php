78b3ab3a0ccb8f8f3888a52a1aa698bd4889865f
Geoffroy Perriard <geoffroy.perriard@liip.ch>
2014-01-14 18:44:21 +0100

    MRP - Add recipes template

---
+++ src/Hoax/CoreBundle/Controller/RecipeController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Hoax\CoreBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+use Doctrine\Common\Persistence\ObjectRepository;
+
+class RecipeController extends Controller
+{
+    /**
+     * Return all the recipes
+     *
+     * @return Response
+     */
+    public function indexAction()
+    {
+        $recipes = $this->getRecipeRepository()->findAll();
+
+        return $this->render(
+            'HoaxCoreBundle:Recipe:index.html.twig',
+            array(
+                'recipes' => $recipes
+            )
+        );
+    }
+
+    /**
+     * Return the Recipe repository
+     * @return ObjectRepository
+     */
+    protected function getRecipeRepository()
+    {
+        return $this->getDoctrine()->getRepository('HoaxCoreBundle:Recipe');
+    }
+}
