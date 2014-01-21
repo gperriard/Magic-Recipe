68c21e7b706803fe2eb982c160851b641308ccb9
Geoffroy Perriard <geoffroy.perriard@liip.ch>
2014-01-21 08:20:48 +0100

    MRP - Add method and form to add an ingredient

--- src/Hoax/CoreBundle/Controller/IngredientController.php
+++ src/Hoax/CoreBundle/Controller/IngredientController.php
@@ -4,8 +4,13 @@ namespace Hoax\CoreBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 
 use Doctrine\Common\Persistence\ObjectRepository;
+
+use Hoax\CoreBundle\Entity\Ingredient;
+use Hoax\CoreBundle\Form\IngredientType;
 
 class IngredientController extends Controller
 {
@@ -22,6 +27,36 @@ class IngredientController extends Controller
             'HoaxCoreBundle:Ingredient:index.html.twig',
             array(
                 'ingredients' => $ingredients
+            )
+        );
+    }
+
+    /**
+     * Display form to add an ingredient
+     *
+     * @param Request $request
+     * @return Response|RedirectResponse
+     */
+    public function newAction(Request $request)
+    {
+        $ingredient = new Ingredient();
+
+        $form = $this->createForm(new IngredientType(), $ingredient);
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($ingredient);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('ingredients'));
+        }
+
+        return $this->render(
+            'HoaxCoreBundle:Ingredient:new.html.twig',
+            array(
+                'form' => $form->createView()
             )
         );
     }
---
+++ src/Hoax/CoreBundle/Form/IngredientType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Hoax\CoreBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class IngredientType extends AbstractType
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add(
+            'name',
+            'text',
+            array(
+                'label'  => 'Name',
+            )
+        );
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Hoax\CoreBundle\Entity\Ingredient',
+        ));
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getName()
+    {
+        return 'ingredient';
+    }
+}
